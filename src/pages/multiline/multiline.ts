df968e5020090b1ea800edb3355eb1d4e50f8435
Jo Z <yoyo@Jo-ZdeMacBook-Air.locaill>
2017-08-23 15:39:39 +1200

    second commit

---
+++ src/pages/multiline/multiline.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+	selector: 'page-multiline',
+    templateUrl: 'multiline.html'
+})
+export class MultilinePage { }
